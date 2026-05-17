e6571de514d943c4987282c2dc4ef0db00c7075a
IvanOhorodnyk <ivan.ohorodnyk.pp.2023@lpnu.ua>
2026-05-17 17:00:00 +0300

    fix: add dist to eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import js from "@eslint/js";
 export default [
     js.configs.recommended,
     {
-        ignores: ["coverage/**", "node_modules/**", "e2e/**"]
+        ignores: ["dist/**", "coverage/**", "node_modules/**", "e2e/**"]
     },
     {
         languageOptions: {
